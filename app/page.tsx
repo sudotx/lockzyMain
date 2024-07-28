33261b0b86ba8a850b3145921c6334d667d25c0f
sudotx <64108848+sudotx@users.noreply.github.com>
2024-07-28 23:23:21 +0100

    chore: add to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import { PatientForm } from "@/components/forms/PatientForm";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Home = ({ searchParams }: SearchParamProps) => {
   return (
     <div className="flex h-screen max-h-screen">
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <PatientForm />
+          {/* <PatientForm /> */}
+          <section className="mb-12 space-y-4">
+            <h1 className="header">Welcome To Lockzy 🔓</h1>
+          </section>
+
+          <div className="space-y-4">
+            <Link href="/signin">
+              <Button className="w-full flex rounded-md border border-dark-500 bg-dark-400">
+                Sign In
+              </Button>
+            </Link>
+            <Link href="/register">
+              <Button className="w-full" variant="outline">
+                Get Started
+              </Button>
+            </Link>
+          </div>
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -14,6 +39,13 @@ const Home = ({ searchParams }: SearchParamProps) => {
           </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
     </div>
   );
 };
